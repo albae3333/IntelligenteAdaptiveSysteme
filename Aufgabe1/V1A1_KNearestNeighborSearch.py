88c4b02b4930ad2e9660171d6dede5623c43dbbc
Malte Hoffmann <hoffmannMalte@gmx.de>
2019-10-31 19:01:06 +0100

    finished nr.1

--- Aufgabe1/V1A1_KNearestNeighborSearch.py
+++ Aufgabe1/V1A1_KNearestNeighborSearch.py
@@ -1,7 +1,8 @@
 # Programmgeruest zu Versuch 1, Aufgabe 1
 import numpy as np
 
-def getKNearestNeighbors(x,X,k=1):  # realizes nearest neighbor search of x in database
+
+def getKNearestNeighbors(x, X, k=1):  # realizes nearest neighbor search of x in database
     """
     compute the k nearest neighbors for a query vector x given a data matrix X
     :param x: the query vector x
@@ -9,28 +10,32 @@ def getKNearestNeighbors(x,X,k=1):  # realizes nearest neighbor search of x in d
     :param k: number of nearest-neighbors to be returned
     :return: return list of k line indixes referring to the k nearest neighbors of x in X
     """
-    d=[]                   # REPLACE! compute list of Euklidean distances between x and X[i]
-    return k*[0]           # REPLACE! return indexes of k smallest distances     
+
+    return np.argsort([np.sum(np.power((x - X[i]), 2)) for i in range(len(X))])
+    # k hat O(n) Einfluss auf Laufzeit
+    # len(x) hat O(n) Einfluss
+    # len(X) hat O()
 
 # ***** MAIN PROGRAM ********
 
 # (i) Generate dummy data 
-X = np.array([[1,2,3],[2,3,4],[3,4,5],[4,5,6]]);      # data matrix X: list of data vectors (=database) of dimension D=3
-x = np.array([1.5,3.6,5.7]);                          # a test data vector
-print("Data matrix X=\n",X)
-print("Test vector x=",x)
+X = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6]])  # data matrix X: list of data vectors (=database) of dimension D=3
+x = np.array([1.5, 3.6, 5.7])  # a test data vector
+print("Data matrix X=\n", X)
+print("Test vector x=", x)
 
 # (ii) Print all Euklidean distances to test vector x
-print("Euklidean distances to x: ", [0 for i in range(len(X))])  # REPLACE! compute list of Euklidean distances   
+print("Euklidean distances to x: ",
+      [np.sum(np.power((X[i] - x), 2)) for i in range(len(X))])  # REPLACE! compute list of Euklidean distances
 
 # (iii) Search for k nearest neighbor
-k=2
-idx_knn = getKNearestNeighbors(x,X,k)                  # get indexes of k nearest neighbors
-print("idx_knn=",idx_knn)
+k = 2
+idx_knn = getKNearestNeighbors(x, X, k)  # get indexes of k nearest neighbors
+print("idx_knn=", idx_knn)
 
 # (iv) output results
 print("The k Nearest Neighbors of x are the following vectors:")
 for i in range(k):
-    idx=idx_knn[i]
-    print("The", i+1, "th nearest neighbor is: X[",idx,"]=",X[idx]," with distance ", np.linalg.norm(X[idx]-x))
+    idx = idx_knn[i]
+    print("The", i + 1, "th nearest neighbor is: X[", idx, "]=", X[idx], " with distance ", np.linalg.norm(X[idx] - x))
 
